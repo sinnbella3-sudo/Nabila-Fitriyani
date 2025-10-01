4c6c54d7b0c1432e86519cf7f5886f03cc3361fc
Nabila Fitriyani <sinnbella3@gmail.com>
2025-10-01 10:43:16 +0700

    Add files via upload

---
+++ numerikk.py
@@ -0,0 +1,9 @@
+a = 5j
+b = 10j
+c = a + b
+
+print(a, '+', b, '=', c)
+print(type(a))
+print(type(b))
+print(type(c))
+# output: 5j + 10j = 15j
---
+++ tugas2.py
@@ -0,0 +1,8 @@
+nama_depan = "nabila"
+nama_belakang = "Fitriyani"
+umur = '19'
+alamat = 'Gabek'
+kata_mutiara = "kamu akan menemukan kekuatan dan percaya kamu bisa melakukannya"
+print(nama_depan, '(' + umur + ')', ',', 'dari',alamat, ', kata mutiara:', 'kata_mutiara')
+
+# output: nabila (19) , dari Gabek , kata mutiara: kamu akan menemukan kekuatan dan percaya kamu bisa melakukannya
