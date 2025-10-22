abe0e04d1fe7b47949743734c02fd586b1d91376
Nabila Fitriyani <sinnbella3@gmail.com>
2025-10-22 11:11:16 +0700

    Add files via upload

---
+++ perusahaan = 'Microsoft'.py
@@ -0,0 +1,24 @@
+perusahaan = 'Microsoft'
+list_pulau = ['Jawa', 'Sumatra', Sulawesi']
+
+# ini adalah dictionary, insyaallah akan kita pelajari
+# di pertemuan-pertemuan yang akan datang
+# mahasiswa = {
+       'nama' : 'Nabila Fitriyani'
+       'asal' : 'Bangka Belitung'
+}
+print (
+    "apakah 'c' ada di variabel perusahaan?",
+)
+print(
+    "apakah 'z' ada di variabel perusahaan?",
+    'c' not it perusahaan
+)
+print(
+    "Apakah 'Madura' tidak ada di variabel list_pulau?",
+    'Madura' not it perusahaan
+)
+print(
+    "Apakah atribut 'nama' ada di variabel mahasiswa?",
+    'nama' in Mahasiswa
+)
